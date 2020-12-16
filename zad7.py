29541c04e5a04df8b4718ec7c33ddf64230d75a3
kappa243 <edek15pl@gmail.com>
2020-12-16 20:06:14 +0100

    zad7

--- zad7.py
+++ zad7.py
@@ -1,8 +1,8 @@
-tab = [[2, 3, 3, 7, 9],
-       [3, 4, 7, 7, 12],
-       [9, 9, 12, 13, 16],
-       [2, 4, 7, 12, 13],
-       [1, 2, 7, 7, 9]]
+tab = [[2,  3,  3,  7,  9],
+       [3,  4,  7,  7,  12],
+       [9,  9,  12, 13, 16],
+       [2,  4,  7,  12, 13],
+       [1,  2,  7,  7,  9]]
 
 
 def convert_tab(tab):
@@ -10,26 +10,26 @@ def convert_tab(tab):
     n_col = [0 for _ in range(n)]
     tab_out = []
 
-    point = 0
-    changed_point = False
-    while not changed_point:
+    smallest_in_row = 0
+    changed_in_row = False
+    while not changed_in_row:
         for row in range(n):
             if n_col[row] < n:
-                if tab[row][n_col[row]] <= tab[point][n_col[point]]:
-                    changed_point = True
-                    point = row
+                if tab[row][n_col[row]] <= tab[smallest_in_row][n_col[smallest_in_row]]:
+                    changed_in_row = True
+                    smallest_in_row = row
 
-        if changed_point:
-            tab_out.append(tab[point][n_col[point]])
-            n_col[point] += 1
-            changed_point = False
+        if changed_in_row:
+            tab_out.append(tab[smallest_in_row][n_col[smallest_in_row]])
+            n_col[smallest_in_row] += 1
+            changed_in_row = False
 
             for row in range(n):
                 if n_col[row] < n:
-                    point = row
+                    smallest_in_row = row
                     break
                 if row == n - 1:
-                    changed_point = True
+                    changed_in_row = True
     return tab_out
 
 
